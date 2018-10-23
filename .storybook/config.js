3015d4c494e9c59170be9f44c81da8314c68e202
Julien Barbay <barbay.julien@gmail.com>
2018-10-23 11:51:08 +0900

    testing storybook@4-rc fresh install

---
+++ .storybook/config.js
@@ -0,0 +1,5 @@
+import { configure } from '@storybook/vue';
+
+configure(() => {
+  require('../stories')
+}, module);
---
+++ stories/index.js
@@ -0,0 +1,20 @@
+import { storiesOf } from '@storybook/vue';
+import { withKnobs, text, button } from '@storybook/addon-knobs';
+
+storiesOf('Knobs demo', module)
+  .addDecorator(withKnobs)
+  .add('foobar', () => ({
+    template: '<span @click="i++">My name is {{ name }} and i am {{ i }} years old.</span>',
+
+    data() { return {
+      name: text('name', 'John Doe'),
+      i: 40,
+    }; },
+
+    beforeCreate() {
+      button('age me', () => this.i++)
+    },
+
+    created() { console.log('created') },
+    destroyed() { console.log('destroyed') },
+  }));
